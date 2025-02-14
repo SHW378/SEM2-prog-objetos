8d8d317dfcc5436a9808d2bf9fe952fe0621c9f2
SHW378 <cesarbrmn@hotmail.com>
2025-02-13 19:09:02 -0600

    1.1

---
+++ Actividad POO/act poo/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Empleados primerempleado = new Empleados();
+        primerempleado.setNombre("Juan");
+        primerempleado.SetApellido("Pérez");
+        primerempleado.setCargo("Gerente");
+        primerempleado.setSalario("5000");
+        Empleados segundoempleado = new Empleados();
+        segundoempleado.setNombre("Ana");
+        segundoempleado.SetApellido("Gómez");
+        segundoempleado.setCargo("Contadora");
+        segundoempleado.setSalario("45000");
+        System.out.println("Empleado: " + primerempleado.getNombre() + " " + primerempleado.getApellido() + ", " + primerempleado.getCargo() + ", " + primerempleado.getSalario());
+        System.out.println("Empleado: " + segundoempleado.getNombre() + " " + segundoempleado.getApellido() + ", " + segundoempleado.getCargo() + ", " + segundoempleado.getSalario());
+    }
+}
---
+++ Actividad POO/act poo/src/Empleados.java
@@ -0,0 +1,38 @@
+public class Empleados {
+    private String Nombre;
+    private String Apellido;
+    private String Cargo;
+    private String Salario;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void SetApellido (String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String Cargo) {
+        this.Cargo = Cargo;
+    }
+
+    public String getSalario() {
+        return Salario;
+    }
+
+    public void setSalario(String Salario) {
+        this.Salario = Salario;
+    }
+}
