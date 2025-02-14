dd80772740975b923bc4ab8d901c9af59054a2af
SHW378 <cesarbrmn@hotmail.com>
2025-02-13 23:56:14 -0600

    1.6

--- proyct random/src/App.java
+++ proyct random/src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         brad primerbrad = new brad();
-        primerbrad.setNombre("Jorge El curioso");
-        primerbrad.setGusto("Gruesos");
+        primerbrad.setNombre("");
+        primerbrad.setGusto("");
         primerbrad.setMedida(15);
-        System.out.println("A " + primerbrad.getNombre() + " le gusta " + primerbrad.getGusto() + " y que midan " + primerbrad.getMedida() + " cm de largo");
+        System.out.println("A " + primerbrad.getNombre() + " le gusta " + primerbrad.getGusto() + " y que midan " + primerbrad.getMedida() + " cm");
     }
 }
