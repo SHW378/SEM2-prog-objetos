92261885ad8e01ba09a19284099b28775f5b2f7c
SHW378 <cesarbrmn@hotmail.com>
2025-02-13 23:54:46 -0600

    1.5

---
+++ proyct random/src/brad.java
@@ -0,0 +1,24 @@
+public class brad {
+    private String nombre;
+    private String gusto; 
+    private double Medida;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getGusto() {
+        return gusto;
+    }
+    public void setGusto(String gusto) {
+        this.gusto = gusto;
+    }
+    public double getMedida() {
+        return Medida;
+    }
+    public void setMedida(double Medida) {
+        this.Medida = Medida;
+    }
+}
