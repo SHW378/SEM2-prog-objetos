68c67d3767be7689e27ef94374939902418c5b9a
SHW378 <cesarbrmn@hotmail.com>
2025-02-19 14:41:55 -0600

    2.21

--- Inscripción de nuevos estudiantes/src/App.java
+++ Inscripción de nuevos estudiantes/src/App.java
@@ -21,7 +21,7 @@ public class App {
             while (true) {
             System.out.println("¿El estudiante ha decidido su carrera? (s/n) ");
             respuesta = sc.nextLine();
-            if (respuesta.equals("s") || respuesta.equals("n")) {
+            if (respuesta.equals("s") || respuesta.equals ("n")) { 
                 break;
             } else {
                 System.out.println("Carácter inválido. Por favor ingrese 's' o 'n'.");
