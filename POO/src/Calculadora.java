67bce2210767b17f9d8152facab6e292b4c4aa30
SHW378 <cesarbrmn@hotmail.com>
2025-02-18 18:35:05 -0600

    con atributos

--- POO/src/Calculadora.java
+++ POO/src/Calculadora.java
@@ -1,4 +1,11 @@
 public class Calculadora {
+    private int operador1;
+    private int operador2;
+    public Calculadora(){}
+    public Calculadora(int operador1, int operador2) {
+        this.operador1 = operador1;
+        this.operador2 = operador2;
+    }
     public void suma (String a, String b) {
         System.out.println("No se puede sumar valores no númericos");
     }
