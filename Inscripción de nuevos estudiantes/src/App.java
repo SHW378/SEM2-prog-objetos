adcb8f47c53e0b2931135597358977299c177954
SHW378 <cesarbrmn@hotmail.com>
2025-02-18 19:39:54 -0600

    InscripcionEstudiantes

---
+++ Inscripción de nuevos estudiantes/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    Scanner sc = new Scanner(System.in);
+    public static class Estudiante {
+        private String nombre;
+        private String apellido;
+        private int edad;
+        private String Carrera; 
+        public Estudiante(String nombre, String apellido, int edad, String carrera) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+            this.Carrera = carrera;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+
+    }
+}
