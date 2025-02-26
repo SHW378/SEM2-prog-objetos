4418e940213031e2637f84de461c613a26346792
SHW378 <cesarbrmn@hotmail.com>
2025-02-25 19:32:08 -0600

    1.4

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -2,15 +2,24 @@ public class Main {
     public static void main(String[] args) {
      Perro perro = new Perro();
      Gato gato = new Gato();
-     //Establece el atributo 'nombre' al perro
+     
+    //Establece el atributo 'nombre' al perro
     perro.setNombre("Firulais");    
     gato.setNombre("Garfield");
 
-     //Imprime el nombre del perro
+    //Imprime el nombre del perro
     System.out.println(perro.getNombre());
     System.out.println(gato.getNombre());
 
+    //Usar los métodos heredados
     perro.hacerSonido();
     gato.hacerSonido();
+
+    // Crear el objetos de AccionesAnimal
+    AccionesAnimal accion = new AccionesAnimal();
+
+    //Iteración entre clases
+    accion.dormir(perro.getNombre());
+    accion.dormir(gato.getNombre());
     }
 }
