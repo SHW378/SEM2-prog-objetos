95ee865a63b5ee3de963e6f1b1a8e58b22598c99
SHW378 <cesarbrmn@hotmail.com>
2025-02-25 18:45:41 -0600

    1

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 public class Main {
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
      Perro perro = new Perro();
      
      //Establece el atributo 'nombre' al perro
-     perro.setNombre("firulais");
+    perro.setNombre(null);    
 
      //Imprime el nombre del perro
-     System.out.println(perro.getNombre());
+    System.out.println(perro.getNombre());
     }
 }
