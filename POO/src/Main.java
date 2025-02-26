d9f36ab3d1da75b37bb738fff5d96715270489d4
SHW378 <cesarbrmn@hotmail.com>
2025-02-25 19:26:01 -0600

    1.31

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
     public static void main(String[] args) {
      Perro perro = new Perro();
-     
+     Gato gato = new Gato();
      //Establece el atributo 'nombre' al perro
-    perro.setNombre("julian");    
+    perro.setNombre("Firulais");    
+    gato.setNombre("Garfield");
 
      //Imprime el nombre del perro
     System.out.println(perro.getNombre());
+    System.out.println(gato.getNombre());
+
+    perro.hacerSonido();
+    gato.hacerSonido();
     }
 }
