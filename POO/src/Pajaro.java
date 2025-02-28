7d41173941b5400dbb2d52ab32a988fee5f00803
SHW378 <cesarbrmn@hotmail.com>
2025-02-27 19:25:19 -0600

    pajaro y volador

---
+++ POO/src/Pajaro.java
@@ -0,0 +1,6 @@
+public class Pajaro implements Volador {
+        public void volar() {
+            // Aquí el pájaro está implementando el método volar a su manera
+            System.out.println("El pájaro vuela");
+        }
+}
