79d5fd787263ee2d4d6502bd727b9ef36b12557a
SHW378 <cesarbrmn@hotmail.com>
2025-03-20 22:28:20 -0600

    promediopremium😎

--- SacarPromedio/src/App.java
+++ SacarPromedio/src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class App {
     public static void main(String[] args) {
@@ -8,12 +9,31 @@ public class App {
         String decision;
 
         do {
-            System.out.print("Ingrese un número: ");
-            double numero  = sc.nextDouble();
+            double numero = 0;
+            boolean caracterValido = false;
+            while(!caracterValido) {
+                try {
+                    System.out.println("Ingrese un número: ");
+                    numero = sc.nextDouble();
+                    if (numero <= -1) {
+                        System.out.println("Error: ingrese un número positivo");
+                    } else {
+                        caracterValido = true;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Error: debe ingresar un número");
+                    sc.next();
+                }
+            }
             suma += numero;
             cantidad++;
-            System.out.println("desea ingresar otro numero? (s/n)");
+            System.out.println("¿Desea ingresar otro numero? (s/n)");
             decision = sc.next();
+            while(!decision.equals("s") && !decision.equals("n")) {
+                System.out.println("Error: ingrese s o n");
+                System.out.println("¿Desea ingresar otro número? (s/n)");
+                decision = sc.next();
+            }
         } while (decision.equals("s"));
         
         if (decision.equals("n")) {
