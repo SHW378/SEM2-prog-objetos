e87b8cff412a956be763b1417ecc23c1397449ae
SHW378 <cesarbrmn@hotmail.com>
2025-04-10 20:19:17 -0600

    mas metodos

--- EstructurasDatos/EstructuraDatos/src/ListaEnlazadaSimple.java
+++ EstructurasDatos/EstructuraDatos/src/ListaEnlazadaSimple.java
@@ -12,4 +12,36 @@ public class ListaEnlazadaSimple {
         nuevoNodo.puntero = cabecera;
         cabecera = nuevoNodo;
     }
+
+    //Método para agregar un elemento al final de la lista 
+    public void agregarAlfinal(int dato){
+        Nodo nuevoNodo = new Nodo(dato);
+        if (cabecera == null) {
+            // La lista está vacia
+            cabecera = nuevoNodo;
+        } else {
+            Nodo nodoTemporal = cabecera;
+            // Recorrer la lista hasta el último nodo 
+            while (nodoTemporal.puntero != null){
+                nodoTemporal = nodoTemporal.puntero;
+            }
+            // Asinar el nuevo nodo al final de la lista 
+            nodoTemporal.puntero = nuevoNodo;
+        }
+    }
+
+    // Método para verificar si la lista está vacia
+    public boolean estaVacia() {
+        return cabecera == null;
+    }
+
+    // Método para imprimir los elementos de la lista 
+    public void imprimirLista() {
+        Nodo nodoActual = cabecera;
+        while (nodoActual != null){
+            System.out.println(nodoActual.dato + " -> ");
+            nodoActual = nodoActual.puntero;
+        }
+        System.out.print("null");
+    }
 }
