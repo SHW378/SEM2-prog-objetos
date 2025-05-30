03e7f56d3ec2993000291b8d3290c3f197e9b14f
SHW378 <cesarbrmn@hotmail.com>
2025-05-29 23:49:55 -0600

    Nodo

---
+++ DataStructure/src/Nodo.java
@@ -0,0 +1,13 @@
+public class Nodo {
+    int dato;
+    Nodo puntero;
+
+    
+    
+    // constructor para inicializar el nodo
+    public Nodo(int dato){
+        this.dato = dato;
+        this.puntero = null;
+    }
+    
+}
