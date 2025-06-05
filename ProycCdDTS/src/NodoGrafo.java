9d50a4ea4cfbf5b48a0cc00f99d1312cfab3455e
SHW378 <cesarbrmn@hotmail.com>
2025-06-05 16:09:29 -0600

    quito clases innecesarias

--- ProycCdDTS/src/NodoGrafo.java
+++
@@ -1,9 +0,0 @@
-public class NodoGrafo {
-    String destino;
-    NodoGrafo siguiente;
-
-    public NodoGrafo(String destino, NodoGrafo siguiente) {
-        this.destino = destino;
-        this.siguiente = siguiente;
-    }
-}
